1258531d3d12e621d6c322ce2a0d16af55eb6bfe
asheldo <andy.m.sheldon@gmail.com>
2015-05-31 08:11:10 -0400

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,10 +1,10 @@
 (defproject atw-om/lein-template "0.3.1"
-  :description "Clojure, all the way with Om"
-  :url "https://github.com/zaiste/atw-om"
+  :description "Clojure, all the way with Om, based on zaiste/atw-om"
+  :url "https://github.com/asheldo/async-om"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :scm {:name "git"
-        :url "https://github.com/zaiste/atw-om"}
-  :signing {:gpg-key "oh@zaiste.net"}
+        :url "https://github.com/asheldo/async-om"}
+  :signing {:gpg-key "andymsheldon@gmail.com"}
   :deploy-repositories [["clojars" {:creds :gpg}]]
   :eval-in-leiningen true)
